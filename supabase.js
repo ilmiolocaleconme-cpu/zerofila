8e2c05dce7627e4259c2f7278379cbff0abc5dfc
ilmiolocaleconme-cpu <ilmiolocaleconme@gmail.com>
2026-05-14 17:23:16 +0200

    supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://mllmvjaiaqdurbnqyonh.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_YJtWbi_UYBXqldQSE8pmmg_7JXexbfn";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
