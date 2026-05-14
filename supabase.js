16994ab26c0dc435053591581a32009f1c01cba7
ilmiolocaleconme-cpu <ilmiolocaleconme@gmail.com>
2026-05-14 17:43:35 +0200

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://mllmvjaiaqdurbnqyonh.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sbG12amFpYXFkdXJibnF5b25oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NjMzNjgsImV4cCI6MjA5NDMzOTM2OH0.RlSYvpx_hA27V7y2fTOBknf92LZeAjbARfcjActilyE";
+const SUPABASE_ANON_KEY = "sb_publishable_YJtWbi_UYBXqldQSE8pmmg_7JXexbfn";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
