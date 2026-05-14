9c91ae320ea2a14612af033f56d09e95778c031a
ilmiolocaleconme-cpu <ilmiolocaleconme@gmail.com>
2026-05-14 19:11:21 +0200

    cart.js

--- cart.js
+++ cart.js
@@ -58,7 +58,7 @@ document
     messaggio += `%0A Totale: € ${totale}`;
 
     window.open(
-      `https://wa.me/393896180004?text=${messaggio}`,
+      `https://wa.me/3896190004?text=${messaggio}`,
       "_blank"
     );
 });
