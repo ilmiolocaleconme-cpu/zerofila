d2503b6996f3373d24a1954e58646de930d22359
ilmiolocaleconme-cpu <ilmiolocaleconme@gmail.com>
2026-05-17 14:54:05 +0200

    supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,9 @@
-const SUPABASE_URL = "https://mllmvjaiaqdurbnqyonh.supabase.co";
+// supabase.js
+const SUPABASE_URL = "https://mllmvjaiaqdurbnqyonh.supabase.co"; 
+const SUPABASE_KEY = "sb_publishable_YJtWbi_UYBXqldQSE8pmmg_7JXexbfn";
 
-const SUPABASE_ANON_KEY = "sb_publishable_YJtWbi_UYBXqldQSE8pmmg_7JXexbfn";
+if (typeof supabase === 'undefined') {
+    console.error("Errore Critico: SDK di Supabase non caricato nel DOM.");
+}
 
-const supabaseClient = supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+export const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
