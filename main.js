e48eaceef854065b5dcb622fbec5ab3053357574
ilmiolocaleconme-cpu <ilmiolocaleconme@gmail.com>
2026-05-21 21:38:36 +0200

    main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", async () => {
     if (window.location.pathname.includes("kitchen.html")) {
-        const kitchenMod = await import('./kitchen.js?v=60');
+        const kitchenMod = await import('./kitchen.js?v=61');
         if (kitchenMod && typeof kitchenMod.initKitchen === "function") {
             kitchenMod.initKitchen();
         }
     } else {
-        const menuMod = await import('./menu.js?v=60');
+        const menuMod = await import('./menu.js?v=61');
         if (menuMod && typeof menuMod.initMenu === "function") {
             menuMod.initMenu();
         }
